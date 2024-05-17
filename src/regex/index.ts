5c0af61d2112b611b34873ed9ff5f1f323891d6b
pranshu <srivastavapranshu2@gmail.com>
2024-05-18 02:06:45 +0530

    changed regex for password

--- src/regex/index.ts
+++ src/regex/index.ts
@@ -1,7 +1,7 @@
 const regexPatterns = {
   emailRegex: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   nameRegex: /^[a-zA-Z]+(?:[' -][a-zA-Z]+)*$/,
-  passwordRegex: /^.{8,10}$/,
+  passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{10,20}$/,
 };
 
 export default regexPatterns;

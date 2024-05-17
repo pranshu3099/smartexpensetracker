2a95d46dd6d6c691ddab8825d2ab5fc00047fb86
pranshu <srivastavapranshu2@gmail.com>
2024-05-18 02:07:56 +0530

    changed schema for name and password

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,11 +1,11 @@
 const { z, string } = require("zod");
 import regexPatterns from "../regex/index";
 const registerSchema = z.object({
-  name: string().min(5).max(100).regex(regexPatterns?.nameRegex, {
+  name: string().min(3).max(100).regex(regexPatterns?.nameRegex, {
     message: "name should contain only alphabets, hyphen or apostrophe",
   }),
-  mobile_number: string().min(8).max(10).regex(regexPatterns?.passwordRegex, {
-    message: "The length of password must be of 8 to 10",
+  password: string().min(10).max(20).regex(regexPatterns?.passwordRegex, {
+    message: "The length of password must be of 10 to 20",
   }),
   email: string()
     .max(100)

3452733f83e0d7b1191dbf0c9a78d482c1488ee1
pranshu <srivastavapranshu2@gmail.com>
2024-05-19 01:09:04 +0530

    added login schema

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -12,5 +12,14 @@ const registerSchema = z.object({
     .regex(regexPatterns?.emailRegex, { message: "Invalid email" }),
 });
 
-const schema = { registerSchema };
+const LoginSchema = z.object({
+  password: string().min(10).max(20).regex(regexPatterns?.passwordRegex, {
+    message: "The length of password must be of 10 to 20",
+  }),
+  email: string()
+    .max(100)
+    .regex(regexPatterns?.emailRegex, { message: "Invalid email" }),
+});
+
+const schema = { registerSchema, LoginSchema };
 export default schema;

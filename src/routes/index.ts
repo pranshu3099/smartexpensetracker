7e3959374ecc977fb79711147518698e39d4d606
pranshu <srivastavapranshu2@gmail.com>
2024-05-28 23:24:47 +0530

    added new route to add expense data

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,29 @@
 import express from "express";
 import UserController from "../controller/UserController";
+import TaskController from "../controller/TaskController";
 import validate from "../middleware/validate";
 import schema from "../schema/schema";
 import verifyToken from "../middleware/Authmiddleware";
 import { customRequest } from "../types/types";
-import { db } from "../db/connection";
 const router = express.Router();
 
 router.post(
   "/v1/user/signup",
   validate({ body: schema?.registerSchema }),
-  (req, res, next) => {
-    UserController.RegisterUser(req, res, next, db);
-  }
+
+  UserController.RegisterUser
 );
 
 router.post(
   "/v1/user/login",
   validate({ body: schema?.LoginSchema }),
-  (req, res, next) => {
-    UserController.LoginUser(req, res, next, db);
-  }
+  UserController.LoginUser
+);
+
+router.post(
+  "/v1/user/addexpenseitem",
+  validate({ body: schema.ExpenseSchema }),
+  TaskController.AddExpenseData
 );
 
 export default router;

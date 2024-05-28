98e0dc30b24d2d33e5869e5708caf2d3cf862b85
pranshu <srivastavapranshu2@gmail.com>
2024-05-28 13:44:33 +0530

    removed route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import UserController from "../controller/UserController";
 import validate from "../middleware/validate";
 import schema from "../schema/schema";
 import verifyToken from "../middleware/Authmiddleware";
-import CompanyController from "../controller/CompanyController";
 import { customRequest } from "../types/types";
 import { db } from "../db/connection";
 const router = express.Router();
@@ -23,9 +22,5 @@ router.post(
     UserController.LoginUser(req, res, next, db);
   }
 );
-
-router.post("/v1/company/register", verifyToken, (req, res, next) => {
-  CompanyController.RegisterCompanyData(req as customRequest, res, next, db);
-});
 
 export default router;

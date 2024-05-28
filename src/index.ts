26694302cbf4f5195369c0009934c7b83adb56ad
pranshu <srivastavapranshu2@gmail.com>
2024-05-28 13:40:54 +0530

    added port env variable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 require("dotenv").config();
 import express from "express";
 import routes from "./routes/index";
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("starting node server");
 });

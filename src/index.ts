6364f46713311d1beaf72233eba2f21d2aa75242
pranshu <srivastavapranshu2@gmail.com>
2024-07-21 06:24:45 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 require("dotenv").config();
 import express from "express";
 import routes from "./routes/index";
+import cors from "cors";
+import cookies from "cookies";
 const port = process.env.PORT;
 const app = express();
+app.use(cookies.express([""]));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(routes);
+
 app.listen(port, () => {
   console.log("starting node server");
 });

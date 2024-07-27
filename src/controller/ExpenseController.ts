622d27f7e787dd4d9d2991e1cfda980e180a48eb
pranshu <srivastavapranshu2@gmail.com>
2024-07-28 02:27:37 +0530

    added ExpensesAnalysisData to get analatics for the user

--- src/controller/ExpenseController.ts
+++ src/controller/ExpenseController.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import { getDb } from "../db/connection";
 import utils from "../utils/utils";
+import { timeStamp } from "console";
+import { ObjectId } from "mongodb";
+
+type Expense = {
+  amount: number;
+};
+
 const AddExpenseData = async (
   req: express.Request,
   res: express.Response,
@@ -9,7 +16,7 @@ const AddExpenseData = async (
   const { amount, email, category, payment_mode, message } = req?.body;
   try {
     const db = getDb();
-    const existingUser = await utils.getUser(email);
+    const existingUser = await utils.getUser(email, {});
 
     if (existingUser) {
       const newDocument = {
@@ -34,6 +41,7 @@ const AddExpenseData = async (
         .json({ error: "conflict", message: "User does not exists" });
     }
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: "Internal server error", err });
   }
 };
@@ -49,8 +57,50 @@ const getExpenseData = async (
 
     return res.status(200).json({ message: "success", getData });
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: "Internal server error", err });
   }
 };
 
-export default { AddExpenseData, getExpenseData };
+const ExpensesAnalysisData = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    let { userId, year, month } = req?.params;
+    const db = getDb();
+    const startDate = new Date(Number(year), Number(month) - 1, 1);
+    const endDate = new Date(Number(year), Number(month), 0);
+
+    const userCollection = db.collection("expense");
+    const expenses = await userCollection
+      .find({
+        user_id: new ObjectId(userId),
+        timeStamp: { $gte: startDate, $lt: endDate },
+      })
+      .toArray();
+    const { user_id } = expenses?.[0];
+    const user = await utils.getUser("", user_id);
+    const { income_per_month } = user;
+    const totalExpenses = expenses.reduce(
+      (acc: number, expense: Expense) => acc + Number(expense.amount),
+      0
+    );
+    const IncomePerMOnth = income_per_month;
+    const remainingBudget = IncomePerMOnth - totalExpenses;
+    const daysInMonth = new Date(Number(year), Number(month), 0).getDate();
+    const dailyBudget = remainingBudget / daysInMonth;
+    const data = {
+      totalExpenses,
+      remainingBudget,
+      dailyBudget,
+    };
+    return res.status(200).json({ message: "success", data });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal server error", err });
+  }
+};
+
+export default { AddExpenseData, getExpenseData, ExpensesAnalysisData };
